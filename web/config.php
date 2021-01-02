f5906d999ca9756cfd559e26ead7998905267a24
Chayapol Moemeng <mchayapol@scitech.au.edu>
2021-01-02 11:35:26 +0700

    Work

---
+++ web/config.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+$dbname = "employees";
+
+// $servername = "us-cdbr-east-02.cleardb.com";
+// $username = "b4c7ffe5a5a37c";
+// $password = "218c61d0";
+// $dbname = "heroku_07c44eac08d0ae1";
+//mysql://b4c7ffe5a5a37c:218c61d0@us-cdbr-east-02.cleardb.com/heroku_07c44eac08d0ae1?reconnect=true
