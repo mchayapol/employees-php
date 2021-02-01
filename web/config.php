8334901bf60597cf431467c4e8b2f8a2a35b4cfe
Chayapol Moemeng <mchayapol@scitech.au.edu>
2021-02-01 11:15:49 +0700

    Removed hard coding

--- web/config.php
+++ web/config.php
@@ -4,8 +4,17 @@
 // $password = "admin";
 // $dbname = "employees";
 
-$servername = "us-cdbr-east-02.cleardb.com";
-$username = "b4c7ffe5a5a37c";
-$password = "218c61d0";
-$dbname = "heroku_07c44eac08d0ae1";
-//mysql://b4c7ffe5a5a37c:218c61d0@us-cdbr-east-02.cleardb.com/heroku_07c44eac08d0ae1?reconnect=true
+// HARD CODING
+// mysql://b4c7ffe5a5a37c:218c61d0@us-cdbr-east-02.cleardb.com/heroku_07c44eac08d0ae1?reconnect=true
+// $servername = "us-cdbr-east-02.cleardb.com";
+// $username = "b4c7ffe5a5a37c";
+// $password = "218c61d0";
+// $dbname = "heroku_07c44eac08d0ae1";
+
+$url = parse_url(getenv("DATABASE_URL"));
+$servername = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$dbname = substr($url["path"], 1);
+
+echo "Extracted Info $servername $username $password $dbnane<br>";
